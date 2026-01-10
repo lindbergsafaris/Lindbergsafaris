e5b90c905f29bffb591cdd417b108aa9cede665b
francis-auka <francisauka7@gmail.com>
2026-01-10 18:48:38 +0300

    fix: remove invalid deployment property from sanity.cli.ts

--- sanity-studio/sanity.cli.ts
+++ sanity-studio/sanity.cli.ts
@@ -4,12 +4,5 @@ export default defineCliConfig({
   api: {
     projectId: 'tdpau1kt',
     dataset: 'production'
-  },
-  deployment: {
-    /**
-     * Enable auto-updates for studios.
-     * Learn more at https://www.sanity.io/docs/cli#auto-updates
-     */
-    autoUpdates: true,
   }
 })
